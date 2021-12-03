ea334899c72f54140510561f3de99dfda02ec2f6
IncroyablePix <bzil714@gmail.com>
2021-12-03 02:37:47 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AdWinistrator
+
+## Abstract
+A few functions for me to administrate terrible Windows Server.
+Should probably not be used by anyone but me unless you know what you're doing.
---
+++ create-user.ps1
@@ -0,0 +1,36 @@
+﻿Function Create-User() {
+    Param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $FullName,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $Name,
+        [Parameter(Mandatory=$true, Position=2)]
+        [string] $Password,
+        [Parameter(Mandatory=$true, Position=3)]
+        [string] $Directory
+    )
+    
+    $user = New-LocalUser -AccountNeverExpires -PasswordNeverExpires 
+    -FullName $FullName -name $Name
+    -Password (ConvertTo-SecureString -AsPlainText $Password -Force)
+    -UserMayNotChangePassword
+
+    # Creating directories
+    [string] $home_dir = "$($Directory)\$($Name)"
+    [string] $profile_dir = "$($home_dir)\ntprof"
+
+    New-Item $home_dir -type directory
+    New-Item $profile_dir -type directory
+
+    # Giving Full control to newly created user
+    $result = &"icacls" $home_dir "/grant" "$($Name):(OI)(CI)(F)"
+    $result = &"icacls" $profile_dir "/grant" "$($Name):(OI)(CI)(F)"
+
+    # ADSI
+    $userADSI = [ADSI] "WinNT://$env:computername/$($Name)"
+    $userADSI.Profile = $profile_dir
+    $userADSI.HomeDirectory = $home_dir
+    $userADSI.SetInfo()
+
+    return $user
+}
